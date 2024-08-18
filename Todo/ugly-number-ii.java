91193f68524fa53acaf2ac127d5f3259895a68ed
Rathod Yuvraj <145740317+rathod-yuvraj@users.noreply.github.com>
2024-08-18 08:06:47 +0530

    Create ugly-number-ii.java

---
+++ Todo/ugly-number-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+  int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;  // The first ugly number is 1
+
+        int i2 = 0, i3 = 0, i5 = 0;  // Pointers for multiples of 2, 3, and 5
+        int nextMultipleOf2 = 2;
+        int nextMultipleOf3 = 3;
+        int nextMultipleOf5 = 5;
+
+        for (int i = 1; i < n; i++) {
+            int nextUgly = Math.min(nextMultipleOf2, Math.min(nextMultipleOf3, nextMultipleOf5));
+            uglyNumbers[i] = nextUgly;
+
+            if (nextUgly == nextMultipleOf2) {
+                i2++;
+                nextMultipleOf2 = uglyNumbers[i2] * 2;
+            }
+            if (nextUgly == nextMultipleOf3) {
+                i3++;
+                nextMultipleOf3 = uglyNumbers[i3] * 3;
+            }
+            if (nextUgly == nextMultipleOf5) {
+                i5++;
+                nextMultipleOf5 = uglyNumbers[i5] * 5;
+            }
+        }
+
+        return uglyNumbers[n - 1];
+    }
+}
